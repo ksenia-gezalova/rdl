15203964ab828c3465da9385c0498cf0ad6e4469
ksenia-gezalova <ksenia.gezalova@gmail.com>
2019-01-15 14:04:49 +0300

    init new project and add global styles

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style land="scss">
+@import "../src/scss/style.scss";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <header class="header__menu">
+      <a href class="header__link">
+        <img src="../assets/RDL-black.png" alt="Logo RDL-Telecom" class="header__logo">
+      </a>
+      <ul class="header__list">
+        <li class="header__item">
+          <a href class="header__link">Главная</a>
+        </li>
+        <li class="header__item">
+          <a href class="header__link">О компании</a>
+        </li>
+        <li class="header__item">
+          <a href class="header__link">Наша команда</a>
+        </li>
+        <li class="header__item">
+          <a href class="header__link">Проекты</a>
+        </li>
+        <li class="header__item">
+          <a href class="header__link">Вакансии</a>
+        </li>
+        <li class="header__item">
+          <a href class="header__link">Контакты</a>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+
+<style lang="scss" scoped>
+.header {
+  &__menu {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 4rem;
+  }
+  &__list {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    align-items: center;
+  }
+  &__logo {
+    width: 195px;
+    margin-right: 12rem;
+  }
+}
+</style>
